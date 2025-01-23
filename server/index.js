dba08e8bf897f3116922dd95e6e001bec21cf576
Hksona123 <sonasingh0307@gmail.com>
2025-01-24 02:22:41 +0530

    Initial commit with all project files

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express= require("express");
+const cors=require("cors");
+const mongoose= require("mongoose");
+
+const app=express();
+require("dotenv").config();
+
+// Route to handle the root URL
+app.get('/', (req, res) => {
+    res.send('Welcome to the Chat App!');
+  });
+
+  
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const server = app.listen(process.env.PORT, ()=>
+{
+    console.log('Server started at Port ${process.env.PORT}');
+});
