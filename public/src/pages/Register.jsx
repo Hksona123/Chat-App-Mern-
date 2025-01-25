2e93634c2b704fd3af1f1ae743f039f8e6780909
Hksona123 <sonasingh0307@gmail.com>
2025-01-25 23:19:41 +0530

    Commit

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import styled from "styled-components"
+import { Link } from "react-router-dom";
 
 function Register() {
+    const handleSubmit=(event)=>{
+        event.preventDefault();
+        alert("form");
+    };
+    const handleChange=(event)=>
+    {
+
+    };
   return (
-    <div><h1>Register</h1></div>
+    <>
+    <FormContainer>
+        <form onSubmit={(event)=>handleSubmit(event)}></form>
+        <div className="brand">
+            <img src="" alt="" />
+            <h1>Chit-Chat</h1>
+        </div>
+        <input type="text" placeholder='Username' name='username' onChange={e=>handleChange(e)}/>
+        <input type="email" placeholder='Email' name='email' onChange={e=>handleChange(e)}/>
+        <input type="password" placeholder='Password' name='password' onChange={e=>handleChange(e)}/>
+        <input type="password" placeholder='Confirm Password' name='confirmPassword' onChange={e=>handleChange(e)}/>
+        <button type="submit">Create User</button>
+        <span>Already have an account ? <Link to="/login">Login</Link></span>
+    </FormContainer>
+    </>
   )
 }
-
+const FormContainer =styled.div``;
 export default Register
