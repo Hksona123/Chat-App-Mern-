a082c2adc9490dcfdcc53a511b4e0dc25f390b83
Hksona123 <sonasingh0307@gmail.com>
2025-01-30 10:50:40 +0530

    Almost done

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,6 +3,11 @@ const bcrypt= require("bcrypt");
 
 module.exports.register =(req,res,next)=> {
     const {username,email,password}= req.body;
-    const usernameCheck = await User.findOne;
+    const usernameCheck = await User.findOne({username});
+    if(usernameCheck)
+        return res.json({msg: "Username already used", status: false});
+    const emailCheck= await User.findOne({email});
+    if(emailCheck)
+        return res.json({msg: "Email already used", status: false});
 };
 
